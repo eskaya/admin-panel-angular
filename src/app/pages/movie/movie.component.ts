6a141cb44b8e0d843d125352b5a291bf45cee8d1
eskaya1 <esra.kaya@naylalabs.com>
2022-04-25 13:00:49 +0300

    Create update api connection.

--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -1,6 +1,7 @@
 import { HttpErrorResponse }                  from '@angular/common/http';
 import { Component, OnInit }                  from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute }                     from '@angular/router';
 import { NbToastrService }                    from '@nebular/theme';
 import { MovieService }                       from '../../services/movie.service';
 
@@ -13,39 +14,52 @@ export class MovieComponent implements OnInit {
     
     public movieForm: FormGroup;
     public isLoading: boolean = false;
+    public _id: string;
     
     constructor(
         private formBuilder: FormBuilder,
         private movieService: MovieService,
         private toastrService: NbToastrService,
+        private route: ActivatedRoute,
     ) { }
     
     ngOnInit(): void {
         this.fnCreateMovieForm();
+        this.route.params.subscribe(params => {
+            if (params.id) {
+                this.movieService.get(params.id).subscribe((res) => {
+                    this._id = params.id;
+                    this.movieForm.patchValue(res);
+                    this.movieForm.markAsPristine();
+                });
+            }
+        });
+        
     }
     
     public fnCreateMovieForm() {
         this.movieForm = this.formBuilder.group({
-            _id       : [],
-            title     : [ '', Validators.required ],
-            imdb_score: [ '', Validators.compose([
+            _id        : [],
+            title      : [ '', Validators.required ],
+            imdb_score : [ '', Validators.compose([
                 Validators.required,
                 Validators.min(0),
+                Validators.max(10),
             ]) ],
-            category  : [ '', Validators.required ],
-            year      : [ '', Validators.compose([
+            category   : [ '', Validators.required ],
+            year       : [ '', Validators.compose([
                 Validators.required,
                 Validators.min(1900),
             ]) ],
-            country   : [ '', Validators.required ],
-            director_id: ['', Validators.required],
+            country    : [ '', Validators.required ],
+            director_id: [ '', Validators.required ],
         });
     }
     
     public clickSubmitButton() {
         const formValue = this.movieForm.value;
-        if (formValue._id) {
-        
+        if(formValue._id) {
+            this.fnUpdateMovie(formValue);
         } else {
             this.fnCreateMovie(formValue);
         }
@@ -57,6 +71,20 @@ export class MovieComponent implements OnInit {
                 this.isLoading = false;
                 this.toastrService.success('Create new movie');
                 this.fnCreateMovieForm();
+            },
+            (httpError: HttpErrorResponse) => {
+                this.isLoading = false;
+                const { error } = httpError;
+                this.toastrService.danger(error);
+            },
+        );
+    }
+    
+    public fnUpdateMovie(form) {
+        this.isLoading = true;
+        this.movieService.update(form, this._id).subscribe((response) => {
+                this.isLoading = false;
+                this.toastrService.success('Success update');
             },
             (httpError: HttpErrorResponse) => {
                 this.isLoading = false;
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,10 +1,10 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Injectable }                    from '@angular/core';
-import { Observable, throwError }        from 'rxjs';
-import { retry, catchError }             from 'rxjs/operators';
-import { getEndpoint }                   from '../../environments/environment';
-import { HttpInterface }                 from '../../interfaces/http.interface';
-import { MovieInterface }                from '../../interfaces/movie.interface';
+import { HttpClient, HttpErrorResponse }      from '@angular/common/http';
+import { Injectable }                         from '@angular/core';
+import { Observable, throwError, observable } from 'rxjs';
+import { retry, catchError }                  from 'rxjs/operators';
+import { getEndpoint }                        from '../../environments/environment';
+import { HttpInterface }                      from '../../interfaces/http.interface';
+import { MovieInterface }                     from '../../interfaces/movie.interface';
 
 @Injectable({
     providedIn: 'root',
@@ -13,10 +13,36 @@ export class MovieService {
     
     constructor(private http: HttpClient) { }
     
+    // Create Movie
     public create(form: MovieInterface): Observable<HttpInterface<MovieInterface>> {
         return this.http
             .post<HttpInterface<MovieInterface>>(getEndpoint('api/movie'), form,
                 { observe: 'body' },
+            )
+            .pipe(
+                retry(1),
+                catchError((error: HttpErrorResponse) => throwError(error.error)),
+            );
+    }
+    
+    // Update Movie
+    public update(form: MovieInterface, _id: string): Observable<HttpInterface<MovieInterface>> {
+        return this.http
+            .patch<HttpInterface<MovieInterface>>(getEndpoint('api/movie/update/') + _id, form,
+                { observe: 'body' },
+            )
+            .pipe(
+                retry(1),
+                catchError((error: HttpErrorResponse) => throwError(error.error)),
+            );
+    }
+    
+    // Movie Detail
+    public get(_id: string, params: any = {}): Observable<HttpInterface<MovieInterface>> {
+        return this.http
+            .get<HttpInterface<MovieInterface>>(getEndpoint('api/movie/detail/') + _id,
+                { observe: 'body' },
+                // if you have param -->     { observe: 'body', params: params.params }
             )
             .pipe(
                 retry(1),
